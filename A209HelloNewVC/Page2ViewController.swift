65c4e992131c549e6b927d7eca05af52ae1b429d
shengyuan wu <pokl50603@gmail.com>
2022-09-17 16:47:22 +0800

    差不多了

---
+++ A209HelloNewVC/Page2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Page2ViewController.swift
+//  A209HelloNewVC
+//
+//  Created by shengyuan on 2022/9/17.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Page2")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- A209HelloNewVC/ViewController.swift
+++ A209HelloNewVC/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var display: UILabel!
+    let dateFormater = DateFormatter()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dateFormater.dateFormat = "yyyy-MMM-dd HH:mm"
+        dateFormater.locale = Locale(identifier: "zh_Hant_TW")
+        let userDefault = UserDefaults.standard
+        if let theDate = userDefault.value(forKey: "lastInputtDate")as? Date{
+            display.text = dateFormater.string(from: theDate)
+        }
+    }
+    @IBAction func dateSelectedAction(_ sender: UIDatePicker){
+        let theDate = sender.date
+        display.text = dateFormater.string(from: theDate)
+        let userDefault = UserDefaults.standard
+        userDefault.set(theDate, forKey: "lastInputDate")
+        userDefault.synchronize()
     }
 
-
 }
-
